03062308e789506eb608977656864dfb328bba17
unknown <geovonnnievasquez@gmail.com>
2021-08-11 23:30:46 -0500

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using LoginAndRegistrationC.Models;
+using Microsoft.AspNetCore.Identity;
+// Other using statements
+namespace LoginAndRegistrationC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+
+        // here we can "inject" our context service into the constructor
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        // success page view
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        // Register Post
+        [HttpPost("register")]
+        public IActionResult Register(User user)
+        {
+            // Check initial ModelState
+            if (ModelState.IsValid)
+            {
+                // If a User exists with provided email
+                if (_context.Users.Any(u => u.Email == user.Email))
+                {
+                    // Manually add a ModelState error to the Email field, with provided
+                    // error message
+                    ModelState.AddModelError("Email", "Email already in use!");
+
+                    // You may consider returning to the View at this point
+                    return View("Index");
+                }
+                // if everything is okay save the user to the DB
+                // Initializing a PasswordHasher object, providing our User class as its type
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user, user.Password);
+                _context.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            // other code
+            return View("Index");
+        }
+
+        // get the Login page
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Login Post 
+        [HttpPost("LoginPost")]
+        public IActionResult LoginPost(LoginUser userSubmission)
+        {
+            if (ModelState.IsValid)
+            {
+                // If inital ModelState is valid, query for a user with provided email
+                var userInDb = _context.Users.FirstOrDefault(u => u.Email == userSubmission.Email);
+                // If no user exists with provided email
+                if (userInDb == null)
+                {
+                    // Add an error to ModelState and return to View!
+                    ModelState.AddModelError("Email", "Invalid Email/Password");
+                    return View("SomeView");
+                }
+
+                // Initialize hasher object
+                var hasher = new PasswordHasher<LoginUser>();
+
+                // verify provided password against hash stored in db
+                var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.Password);
+
+                // result can be compared to 0 for failure
+                if (result == 0)
+                {
+                    // handle failure (this should be similar to how "existing email" is handled)
+                     ModelState.AddModelError("Password", "Not the right password cops are being called!");
+                }
+
+                // If everything is good go to the success view page 
+                return RedirectToAction("Success");
+            }
+            // go back to login if fails
+            return View("Login");
+        }
+
+    }
+}
